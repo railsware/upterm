8bfde8af3984b981b1ba91a07c104338b753f6d9
Volodymyr Shatsky <shockone89@gmail.com>
2015-03-21 19:15:39 +0200

    First OK-looking version.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 var app = require('app');
 var BrowserWindow = require('browser-window');
 
+// process.stdin.setEncoding('utf8');
+// process.stdin.setRawMode(true);
+
+
+// term.pipe(process.stdout);
+// process.stdin.pipe(term);
+
+// term.on('close', function() {
+// process.exit();
+// });
+
 var mainWindow = null;
 
 app.on('ready', function() {
-	mainWindow = new BrowserWindow({
-		width: 1000,
-		height: 800
-	});
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 800
+  });
 
-	mainWindow.loadUrl('file://' + __dirname + '/../index.html');
+  mainWindow.loadUrl('file://' + __dirname + '/../index.html');
 });
