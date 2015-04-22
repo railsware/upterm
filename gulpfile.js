1b8328f24cb1cfe698df4a8a10fc667d2875f22e
Volodymyr Shatsky <shockone89@gmail.com>
2015-04-22 23:50:29 +0300

    Refactor gulp.js.

--- gulpfile.js
+++ gulpfile.js
@@ -3,44 +3,43 @@ var sass = require('gulp-sass');
 var react = require('gulp-react');
 var ts = require('gulp-typescript');
 
-var paths = {
-	target: 'compiled',
+var options = {
+    target: 'compiled',
     source: {
         typeScript: 'src/*.ts',
         sass: 'stylesheets/*.sass',
         react: 'react.js'
+    },
+    config: {
+        sass: {
+            indentedSyntax: true,
+            errLogToConsole: true
+        },
+        typeScript: ts.createProject({
+            typescript: require('typescript'),
+            "target": "ES5",
+            "module": "commonjs",
+            "noImplicitAny": true,
+            "removeComments": true,
+            "preserveConstEnums": true,
+            "sourceMap": true,
+            "out": "terminal.js",
+        })
     }
 };
 
-
-var typeScriptConfig = ts.createProject({
-	typescript: require('typescript'),
-	"target": "ES5",
-	"module": "commonjs",
-	"noImplicitAny": true,
-	"removeComments": true,
-	"preserveConstEnums": true,
-	"sourceMap": true,
-	"out": "terminal.js",
+gulp.task('compile-typescript', function () {
+    return gulp.src(options.source.typeScript).pipe(ts(options.config.typeScript)).pipe(gulp.dest(options.target));
 });
 
-var sassConfig = {
-	indentedSyntax: true,
-    errLogToConsole: true
-};
-
-gulp.task('default', function() {
-    gulp.start('compile-typescript', 'compile-sass', 'compile-react');
-});
-
-gulp.task('compile-typescript', function() {
-	return gulp.src(paths.source.typeScript).pipe(ts(typeScriptConfig)).pipe(gulp.dest(paths.target));
-});
-
-gulp.task('compile-sass', function() {
-    return gulp.src(paths.source.sass).pipe(sass(sassConfig)).pipe(gulp.dest(paths.target));
+gulp.task('compile-sass', function () {
+    return gulp.src(options.source.sass).pipe(sass(options.config.sass)).pipe(gulp.dest(options.target));
 });
 
 gulp.task('compile-react', function () {
-    return gulp.src(paths.source.react).pipe(react()).pipe(gulp.dest(paths.target));
+    return gulp.src(options.source.react).pipe(react()).pipe(gulp.dest(options.target));
+});
+
+gulp.task('default', function () {
+    gulp.start('compile-typescript', 'compile-sass', 'compile-react');
 });
