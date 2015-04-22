022a9b777f3f7bfc3d3e83ba9f684bb2505dce84
Volodymyr Shatsky <shockone89@gmail.com>
2015-04-23 00:22:50 +0300

    Address comments.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var react = require('gulp-react');
-var ts = require('gulp-typescript');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const react = require('gulp-react');
+const ts = require('gulp-typescript');
 
 var options = {
     target: 'compiled',
@@ -28,18 +28,22 @@ var options = {
     }
 };
 
-gulp.task('compile-typescript', function () {
-    return gulp.src(options.source.typeScript).pipe(ts(options.config.typeScript)).pipe(gulp.dest(options.target));
+gulp.task('typescript', function () {
+    return gulp.src(options.source.typeScript)
+               .pipe(ts(options.config.typeScript))
+               .pipe(gulp.dest(options.target));
 });
 
-gulp.task('compile-sass', function () {
-    return gulp.src(options.source.sass).pipe(sass(options.config.sass)).pipe(gulp.dest(options.target));
+gulp.task('sass', function () {
+    return gulp.src(options.source.sass)
+               .pipe(sass(options.config.sass))
+               .pipe(gulp.dest(options.target));
 });
 
-gulp.task('compile-react', function () {
-    return gulp.src(options.source.react).pipe(react()).pipe(gulp.dest(options.target));
+gulp.task('react', function () {
+    return gulp.src(options.source.react)
+               .pipe(react())
+               .pipe(gulp.dest(options.target));
 });
 
-gulp.task('default', function () {
-    gulp.start('compile-typescript', 'compile-sass', 'compile-react');
-});
+gulp.task('default', ['typescript', 'sass', 'react']);
