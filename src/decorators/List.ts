b045f1f9357da44f1bcbf1932a5f537053c60c5b
Volodymyr Shatsky <shockone89@gmail.com>
2015-06-05 20:22:50 +0300

    Require all decorators dynamically.

--- src/decorators/List.ts
+++ src/decorators/List.ts
@@ -1,8 +1,22 @@
 import Base = require('./Base');
-import Json = require('./Json');
-import GitDiff = require('./GitDiff');
 import Invocation = require('../Invocation');
+import fs = require('fs');
+import _ = require('lodash');
 
-var list: Array<{new (invocation: Invocation): Base}> = [Json, GitDiff];
+function isDecorator(fileName: string) {
+    return !_.include(['Base.js', 'List.js'], fileName);
+}
+
+function isJSFile(fileName: string) {
+    return _.endsWith(fileName, '.js');
+}
+
+var list: Array<{new (invocation: Invocation): Base}> =
+    _(fs.readdirSync(__dirname))
+        .filter(isJSFile)
+        .filter(isDecorator)
+        .map((fileName) => { return `./${fileName}`})
+        .map(require)
+        .value();
 
 export = list;
