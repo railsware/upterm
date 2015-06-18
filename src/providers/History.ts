dab211cd4c66bdbd9f807ddcecd210c0a722a773
Volodymyr Shatsky <shockone89@gmail.com>
2015-06-18 21:14:24 +0300

    Score history results.

--- src/providers/History.ts
+++ src/providers/History.ts
@@ -2,23 +2,24 @@ import i = require('../Interfaces');
 import _ = require('lodash');
 import Aliases = require('../Aliases');
 import ExecutionHistory = require('../History');
-var filter: any = require('fuzzaldrin').filter;
+var score: (i: string, m: string) => number = require('fuzzaldrin').score;
 
 class History implements i.AutocompletionProvider {
     getSuggestions(currentDirectory: string, input: i.Parsable) {
         return new Promise((resolve) => {
+            var lexeme = input.getLastLexeme();
+
             var all = _.map(ExecutionHistory.stack, (entry: string) => {
                 return {
                     value: entry,
-                    score: 0,
+                    score: score(entry, lexeme),
                     synopsis: '',
                     description: '',
                     type: 'history'
                 };
             });
 
-
-            resolve(filter(all, input.getLastLexeme(), {key: 'value', maxResults: 30}));
+            resolve(_(all).sortBy('score').reverse().take(10).value());
         });
     }
 }
