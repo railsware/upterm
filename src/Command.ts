56de3eb72e69dbfe3d0a8ddfa9509625d96fdbe6
Volodymyr Shatsky <shockone89@gmail.com>
2015-07-11 14:58:00 +0300

    Fix cd.

--- src/Command.ts
+++ src/Command.ts
@@ -1,4 +1,5 @@
 import fs = require('fs');
+import Path = require('path');
 
 // A class representing built in commands
 class Command {
@@ -6,44 +7,18 @@ class Command {
         if (!args.length) {
             return process.env.HOME;
         }
+        var path = args[0].replace(/^~/, process.env.HOME);
+        var newDirectory = Path.resolve(currentDirectory, path);
 
-        var path = args[0];
-        //FIXME: doesn't work correctly for 'cd /'.
-        var tokens: Array<string> = path.split('/');
-        var firstToken = tokens.shift();
-
-        var newDirectory = currentDirectory;
-
-        switch (firstToken) {
-            case '~':
-                newDirectory = process.env.HOME;
-                break;
-            case '/':
-                newDirectory = '/';
-                break;
-            case '..':
-                var parts: Array<string> = currentDirectory.split('/');
-                newDirectory = parts.slice(0, parts.length - 1).join('/');
-                break;
-            case '.':
-                break;
-            default:
-                newDirectory += `/${firstToken}`;
+        if (!fs.existsSync(newDirectory)) {
+            throw new Error(`The directory ${newDirectory} doesn't exist.`);
         }
 
-        if (tokens.length) {
-            return this.cd(newDirectory, [tokens.join('/')]);
-        } else {
-            if (!fs.existsSync(newDirectory)) {
-                throw new Error(`The directory ${newDirectory} doesn't exist.`);
-            }
-
-            if (!fs.statSync(newDirectory).isDirectory()) {
-                throw new Error(`${newDirectory} is not a directory.`);
-            }
-
-            return newDirectory;
+        if (!fs.statSync(newDirectory).isDirectory()) {
+            throw new Error(`${newDirectory} is not a directory.`);
         }
+
+        return newDirectory;
     }
 
     static isBuiltIn(command: String): any {
