db00abce478a5be4f371a7504ef8bc4b50eeb5ec
joshtoo <joshuaw@easymetrics.com>
2015-09-09 01:06:01 -0500

    Issue #101 Added basic gulp.config file

---
+++ gulp.config.js
@@ -0,0 +1,14 @@
+module.exports = function() {
+    var root = './';
+    var src = './src';
+    var config = {
+
+        /**
+         * Javascript file paths
+         */
+        alljs: [
+            src + '**/*.js',
+            root + '*.js'
+        ]
+    };
+};
