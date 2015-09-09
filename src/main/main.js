3a239384477c4fa7c2a2f3cbe1e78697b05ed1cf
Konstantin Azizov <kazizov@readdle.com>
2015-09-09 16:25:32 +0300

    Add window location remember funciton

--- src/main/main.js
+++ src/main/main.js
@@ -21,8 +21,19 @@ app.on('activate-with-no-open-windows', function () {
 });
 
 function createWindow() {
+	var path = require("path");
+	var fs = require("fs");
+	var initPath = path.join(app.getDataPath(), "init.json");
+	var data;
+	
+	try {
+		data = JSON.parse(fs.readFileSync(initPath, 'utf8'));
+	} catch(e) {
+		console.log(e.toString());
+	}
+	
 	var window = new BrowserWindow({
-		width: 700,
+		width: 700, 
 		height: 450,
 		'web-preferences': {
 			'overlay-scrollbars': true
@@ -31,11 +42,19 @@ function createWindow() {
 		show: false
 	});
 	
+	if(data) {
+		window.setBounds(data.bounds);
+	}
+	
 	window.loadUrl('file://' + __dirname + '/../../index.html');
 	menu.setMenu(app, mainWindow);
 	
-	window.on('closed', function() {
-		window = null;
+	window.on('close', function () {
+		var data = {
+			bounds: mainWindow.getBounds()
+		};
+		
+		fs.writeFileSync(initPath, JSON.stringify(data));
 	});
 	
 	window.webContents.on('did-finish-load', function () {
