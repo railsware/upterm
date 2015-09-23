09f6d867cda3d19bcfe6cb7d21bb8403119a60c9
Volodymyr Shatsky <shockone89@gmail.com>
2015-09-23 09:34:46 +0300

    Format Alias.

--- src/providers/Alias.ts
+++ src/providers/Alias.ts
@@ -6,22 +6,22 @@ var score: (i: string, m: string) => number = require('fuzzaldrin').score;
 
 export default class Alias implements i.AutocompletionProvider {
     async getSuggestions(prompt: Prompt) {
-            if (prompt.getWholeCommand().length > 1) {
-                return [];
+        if (prompt.getWholeCommand().length > 1) {
+            return [];
+        }
+
+        var lastArgument = prompt.getLastArgument();
+
+        var all = _.map(Aliases.aliases, (expanded: string, alias: string) => {
+            return {
+                value: alias,
+                score: 2 * (score(alias, lastArgument) + (score(expanded, lastArgument) * 0.5)),
+                synopsis: expanded,
+                description: `Aliased to “${expanded}”.`,
+                type: 'alias',
             }
+        });
 
-            var lastArgument = prompt.getLastArgument();
-
-            var all = _.map(Aliases.aliases, (expanded: string, alias: string) => {
-                return {
-                    value: alias,
-                    score: 2 * (score(alias, lastArgument) + (score(expanded, lastArgument) * 0.5)),
-                    synopsis: expanded,
-                    description: `Aliased to “${expanded}”.`,
-                    type: 'alias',
-                }
-            });
-
-            return _._(all).sortBy('score').reverse().take(10).value();
+        return _._(all).sortBy('score').reverse().take(10).value();
     }
 }
