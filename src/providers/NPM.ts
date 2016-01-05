e088575d7d4b9ed42317209e920ded52ad81b3f9
Volodymyr Shatsky <shockone89@gmail.com>
2016-01-05 15:36:28 +0200

    Make NPM work with aliases.

--- src/providers/NPM.ts
+++ src/providers/NPM.ts
@@ -8,15 +8,14 @@ var score: (i: string, m: string) => number = require('fuzzaldrin').score;
 
 export default class NPM implements i.AutocompletionProvider {
     async getSuggestions(prompt: Prompt): Promise<Suggestion[]> {
-        const lexemes = prompt.lexemes;
+        const words = prompt.expanded;
 
-        if (lexemes.length !== 3 || !_.eq(_.take(lexemes, 2), ['npm', 'run'])) {
+        if (words.length !== 3 || !_.eq(_.take(words, 2), ['npm', 'run'])) {
             return [];
         }
 
-        const thirdLexeme = lexemes[2];
+        const lastWord = _.last(words);
         const packageFilePath = Path.join(prompt.getCWD(), 'package.json');
-
 
         return new Promise((resolve: (suggestions: Suggestion[]) => Promise<Suggestion[]>) => {
             Utils.ifExists(packageFilePath, () => {
@@ -24,7 +23,7 @@ export default class NPM implements i.AutocompletionProvider {
                     const suggestions = Object.keys(JSON.parse(buffer.toString()).scripts || {}).map(key => {
                             return {
                                 value: key,
-                                score: 2 + score(key, thirdLexeme),
+                                score: 2 + score(key, lastWord),
                                 synopsis: '',
                                 description: '',
                                 type: 'option'
