e2f1397b98c7fdd9730a3c729f4d2525033c1d6a
Volodymyr Shatsky <shockone89@gmail.com>
2016-01-12 23:31:59 +0200

    Replace undefined with null.

--- src/views/StatusLineComponent.ts
+++ src/views/StatusLineComponent.ts
@@ -35,7 +35,7 @@ interface VcsDataProps {
 class VcsDataComponent extends React.Component<VcsDataProps, {}> {
     render() {
         if (!this.props.data.isRepository) {
-            return undefined;
+            return null;
         }
 
         return React.createElement(
