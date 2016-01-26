f0a1334f07270a4d2c936b30bf454e343bf23f8f
Volodymyr Shatsky <shockone89@gmail.com>
2016-01-26 14:32:34 +0200

    Rewrite File.

--- src/plugins/autocompletion_providers/File.ts
+++ src/plugins/autocompletion_providers/File.ts
@@ -1,70 +1,31 @@
-import * as i from "../../Interfaces";
-import * as _ from "lodash";
 import Utils from "../../Utils";
 import PluginManager from "../../PluginManager";
-const score: (i: string, m: string) => number = require("fuzzaldrin").score;
+import {File} from "./Suggestions";
+import {FileInfo} from "../../Interfaces";
 
-function filter(command: string): (value: i.FileInfo, index: number, array: i.FileInfo[]) => boolean {
+// FIXME: make a generic file completion provider with ability to filter the results.
+function filter(command: string): (value: FileInfo, index: number, array: FileInfo[]) => boolean {
     switch (command) {
         case "cd":
-            return (fileInfo: i.FileInfo) => fileInfo.stat.isDirectory();
+            return (fileInfo: FileInfo) => fileInfo.stat.isDirectory();
         default:
-            return (fileInfo: i.FileInfo) => true;
+            return (fileInfo: FileInfo) => true;
     }
 }
 
-function escape(path: string): string {
-    return path.replace(/\s/g, "\\ ");
-}
-
 PluginManager.registerAutocompletionProvider({
-    getSuggestions: async (job): Promise<i.Suggestion[]> => {
+    getSuggestions: async function(job) {
         const prompt = job.prompt;
 
-        if (prompt.expanded.length < 2) {
+        if (!prompt.arguments.length) {
             return [];
         }
 
-        const lastArgument = prompt.lastArgument;
-        const baseName = Utils.baseName(lastArgument);
-        const dirName = Utils.dirName(lastArgument);
-        const searchDirectory = Utils.resolveDirectory(job.directory, dirName);
-        const fileInfos = await Utils.stats(searchDirectory);
+        const relativeSearchDirectory = Utils.dirName(prompt.lastArgument);
+        const fileInfos = await Utils.stats(Utils.resolveDirectory(job.directory, relativeSearchDirectory));
 
-        const all = _.map(fileInfos.filter(filter(prompt.commandName)), (fileInfo: i.FileInfo): i.Suggestion => {
-            /* tslint:disable:no-bitwise */
-            let description = `Mode: ${"0" + (fileInfo.stat.mode & 511).toString(8)}`;
-            let name: string;
-
-            if (fileInfo.stat.isDirectory()) {
-                name = Utils.normalizeDir(fileInfo.name);
-            } else {
-                name = fileInfo.name;
-                description += `; Size: ${Utils.humanFileSize(fileInfo.stat.size)}`;
-            }
-
-            const suggestion: i.Suggestion = {
-                value: escape(name),
-                score: 0,
-                synopsis: "",
-                description: description,
-                type: "file",
-                partial: fileInfo.stat.isDirectory(),
-            };
-
-            if (searchDirectory !== job.directory) {
-                suggestion.prefix = escape(dirName);
-            }
-
-            return suggestion;
+        return fileInfos.filter(filter(prompt.commandName)).map(fileInfo => {
+            return new File(fileInfo, relativeSearchDirectory);
         });
-
-        if (baseName) {
-            all.forEach(suggestion => suggestion.score = score(suggestion.value, baseName));
-        } else {
-            all.forEach(suggestion => suggestion.score = 1);
-        }
-
-        return all;
     },
 });
--- src/plugins/autocompletion_providers/Suggestions.ts
+++ src/plugins/autocompletion_providers/Suggestions.ts
@@ -1,4 +1,7 @@
-import {Suggestion} from "../../Interfaces";
+import {Suggestion, FileInfo} from "../../Interfaces";
+import Utils from "../../Utils";
+import Job from "../../Job";
+import * as Path from "path";
 
 abstract class BaseOption extends Suggestion {
     get type() {
@@ -65,5 +68,52 @@ export class Executable extends Suggestion {
 
     get type() {
         return "executable";
+    }
+}
+
+export class File extends Suggestion {
+    constructor(protected _info: FileInfo, protected relativeSearchDirectory: string) {
+        super();
+    };
+
+    get value() {
+        return this.escape(Path.join(this.relativeSearchDirectory, this.unescapedFileName));
+    }
+
+    get displayValue() {
+        return this.unescapedFileName;
+    }
+
+    get type() {
+        return ["file", this.extension].join(" ");
+    }
+
+    get partial() {
+        return this._info.stat.isDirectory();
+    }
+
+    private escape(path: string): string {
+        return path.replace(/\s/g, "\\ ");
+    }
+
+    private get unescapedFileName(): string {
+        if (this._info.stat.isDirectory()) {
+            return Utils.normalizeDir(this._info.name);
+        } else {
+            return this._info.name;
+        }
+    }
+
+    private get extension(): string {
+        if (this._info.stat.isDirectory()) {
+            return "directory";
+        }
+
+        let extension = Path.extname(this.unescapedFileName);
+        if (extension) {
+            return extension.slice(1);
+        } else {
+            return "unknown";
+        }
     }
 }
