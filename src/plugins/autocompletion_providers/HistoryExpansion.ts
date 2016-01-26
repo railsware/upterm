6d34e399011f93927258b1b8f02ec7295b14e08e
Volodymyr Shatsky <shockone89@gmail.com>
2016-01-26 23:19:25 +0200

    Rewrite HistoryExpansion.

--- src/plugins/autocompletion_providers/HistoryExpansion.ts
+++ src/plugins/autocompletion_providers/HistoryExpansion.ts
@@ -1,38 +1,50 @@
 import * as i from "../../Interfaces";
-import * as _ from "lodash";
 import Job from "../../Job";
 import {isCompleteHistoryCommand, historyReplacement} from "../../CommandExpander";
 import PluginManager from "../../PluginManager";
 
-class HistoryExpansion implements i.AutocompletionProvider {
-    private static descriptions: Dictionary<string> = {
-        "!!": "The previous command",
-        "!^": "The first argument of the previous command",
-        "!$": "The last argument of the previous command",
-        "!*": "All arguments of the previous command",
-    };
+const descriptions: Dictionary<string> = {
+    "!!": "The previous command",
+    "!^": "The first argument of the previous command",
+    "!$": "The last argument of the previous command",
+    "!*": "All arguments of the previous command",
+};
 
-    async getSuggestions(job: Job): Promise<i.Suggestion[]> {
-        const lexeme = job.prompt.lastLexeme;
-
-        return _.map(this.commands(lexeme), (description, command) => {
-            return {
-                value: command,
-                score: 4,
-                synopsis: historyReplacement(command).join(" "),
-                description: description,
-                type: "history-expansion",
-            };
-        });
+class Expansion extends i.Suggestion {
+    constructor(protected _command: string) {
+        super();
     }
 
-    private commands(lexeme: string): Dictionary<string> {
+    get value(): string {
+        return this._command;
+    }
+
+    get synopsis(): string {
+        return historyReplacement(this._command).join(" ");
+    }
+
+    get description(): string {
+        return descriptions[this._command];
+    }
+
+    get type(): string {
+        return "history-expansion";
+    }
+}
+
+class HistoryExpansion implements i.AutocompletionProvider {
+
+    async getSuggestions(job: Job): Promise<i.Suggestion[]> {
+        return this.commands(job.prompt.lastLexeme).map(command => new Expansion(command));
+    }
+
+    private commands(lexeme: string): string[] {
         if (isCompleteHistoryCommand(lexeme)) {
-            return { [lexeme]: HistoryExpansion.descriptions[lexeme] };
+            return [lexeme];
         } else if (lexeme.length === 1 && lexeme.startsWith("!")) {
-            return HistoryExpansion.descriptions;
+            return Object.keys(descriptions);
         } else {
-            return {};
+            return [];
         }
     }
 }
