0d164bb16d5c90ef7679902da4b5793e53543367
Volodymyr Shatsky <shockone89@gmail.com>
2016-02-05 11:07:56 +0200

    Rewrite StatusLineComponent in JSX.

---
+++ src/views/StatusLineComponent.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+const CurrentDirectory = ({ currentWorkingDirectory }: { currentWorkingDirectory: string }) =>
+    <div className="current-directory">{currentWorkingDirectory}</div>;
+
+const VcsDataComponent = ({ data }: { data: VcsData }) => {
+    if (this.props.data.isRepository) {
+        return (
+            <div className="vcs-data">
+                <div className={`status ${data.status}`}>{data.branch}</div>
+            </div>
+        );
+    }
+};
+
+const StatusLine = ({ currentWorkingDirectory, vcsData }: { currentWorkingDirectory: string; vcsData: VcsData }) =>
+    <div className="status-line">
+        <CurrentDirectory currentWorkingDirectory={currentWorkingDirectory}/>
+        <VcsDataComponent data={vcsData}/>
+    </div>;
+
+export default StatusLine;
