6720e4977e7ebd8565c895d96e50c7169526b34b
Volodymyr Shatsky <shockone89@gmail.com>
2016-03-06 19:36:46 +0200

    Generalize the RVM plugin.

--- src/plugins/RVM.ts
+++ src/plugins/RVM.ts
@@ -1,22 +1,56 @@
 import Session from "../Session";
 import PluginManager from "../PluginManager";
+import Utils from "../Utils";
+import * as Path from "path";
 
-/* tslint:disable:max-line-length */
-const bbPath = "/Users/me/.rvm/gems/ruby-2.1.2@brightbytes/bin:/Users/me/.rvm/gems/ruby-2.1.2@global/bin:/Users/me/.rvm/rubies/ruby-2.1.2/bin:/Users/me/.rvm/bin";
+const rvmDirectory = Path.join(Utils.homeDirectory, ".rvm");
+const rubyVersionFileName = ".ruby-version";
+const gemSetNameFileName = ".ruby-gemset";
+
+async function isUnderRVM(directory: string): Promise<boolean> {
+    return await Utils.exists(Path.join(directory, rubyVersionFileName));
+}
+
+/**
+ * Contract: the non-global path should be first.
+ */
+async function gemSetPaths(directory: string, rubyVersion: string): Promise<string[]> {
+    const gemSetNameFilePath = Path.join(directory, gemSetNameFileName);
+
+    const paths = [Path.join(rvmDirectory, "gems", `ruby-${rubyVersion}@global`)];
+
+    if (await Utils.exists(gemSetNameFilePath)) {
+        const gemSetName = (await Utils.readFile(gemSetNameFilePath)).trim();
+        paths.unshift(Path.join(rvmDirectory, "gems", `ruby-${rubyVersion}@${gemSetName}`));
+    }
+
+    return paths;
+}
+
+async function binPaths(directory: string, rubyVersion: string): Promise<string> {
+    return [
+        Path.join(rvmDirectory, "bin"),
+        Path.join(rvmDirectory, "rubies", `ruby-${rubyVersion}`, "bin"),
+        ...(await gemSetPaths(directory, rubyVersion)).map(path => Path.join(path, "bin")),
+    ].join(Path.delimiter);
+}
 
 PluginManager.registerEnvironmentObserver({
-    currentWorkingDirectoryWillChange: (session: Session) => {
-        if (session.directory === "/Users/me/dev/brightbytes/") {
-            session.environment.set("PATH", session.environment.path.replace(bbPath, ""));
+    currentWorkingDirectoryWillChange: async(session: Session) => {
+        if (await isUnderRVM(session.directory)) {
+            session.environment.set("PATH", session.environment.path.split(Path.delimiter).filter(path => !path.includes(".rvm")).join(Path.delimiter));
             session.environment.set("GEM_HOME", "");
             session.environment.set("GEM_PATH", "");
         }
     },
-    currentWorkingDirectoryDidChange: (session: Session) => {
-        if (session.directory === "/Users/me/dev/brightbytes/") {
-            session.environment.set("PATH", bbPath + session.environment.path);
-            session.environment.set("GEM_HOME", "/Users/me/.rvm/gems/ruby-2.1.2@brightbytes");
-            session.environment.set("GEM_PATH", "/Users/me/.rvm/gems/ruby-2.1.2@brightbytes:/Users/me/.rvm/gems/ruby-2.1.2@global");
+    currentWorkingDirectoryDidChange: async (session: Session) => {
+        if (await isUnderRVM(session.directory)) {
+            const rubyVersion = (await Utils.readFile(Path.join(session.directory, rubyVersionFileName))).trim();
+
+            const paths = await gemSetPaths(session.directory, rubyVersion);
+            session.environment.set("PATH", await binPaths(session.directory, rubyVersion) + Path.delimiter + session.environment.path);
+            session.environment.set("GEM_PATH", paths.join(Path.delimiter));
+            session.environment.set("GEM_HOME", paths[0]);
         }
     },
 });
