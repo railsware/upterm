2d974e8664544da4522a3b400dc01f4e65943c16
Volodymyr Shatsky <shockone89@gmail.com>
2016-03-20 15:19:47 +0200

    Remove babel. Fixes #215.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -48,7 +48,6 @@ const options = {
 gulp.task("typescript", function () {
         return gulp.src(options.typeScript.source)
             .pipe($.typescript(options.typeScript.config).on("error", onError))
-            .pipe($.babel().on("error", onError))
             .pipe(gulp.dest(options.typeScript.target))
             .pipe(notify("TypeScript has been compiled."))
     }
--- src/Decorators.ts
+++ src/Decorators.ts
@@ -2,10 +2,7 @@ import * as _ from "lodash";
 
 export function memoize(resolver: Function = undefined) {
     if (typeof resolver !== "function") {
-        // TODO: convert to an arrow function when https://github.com/babel/babel/issues/2332 is closed.
-        resolver = function (...args: any[]) {
-            return JSON.stringify(args);
-        };
+        resolver = (...args: any[]) => JSON.stringify(args);
     }
 
     return (target: any, name: string, descriptor: PropertyDescriptor) => {
