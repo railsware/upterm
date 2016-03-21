e921ad792a2857c7c9671f218f3eb57c4a4630d9
Volodymyr Shatsky <shockone89@gmail.com>
2016-03-21 16:08:02 +0200

    Run tsc correctly.

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ const options = {
     }
 };
 
-gulp.task("typescript", $.shell.task("tsc", {cwd: "/Users/me/dev/black-screen"}));
+gulp.task("typescript", $.shell.task("npm run tsc", {cwd: "/Users/me/dev/black-screen"}));
 
 gulp.task("sass", function () {
         return gulp.src(options.sass.source)
