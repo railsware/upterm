358c1686ad011d0af0402037f1f79c91a1276cb0
Volodymyr Shatsky <shockone89@gmail.com>
2016-05-08 23:00:15 +0300

    Separate grammar creation.

--- src/Autocompletion.ts
+++ src/Autocompletion.ts
@@ -2,68 +2,68 @@ import * as _ from "lodash";
 import Job from "./Job";
 import {
     choice, token, executable, decorate, sequence, string, many1,
-    optionalContinuation, spacesWithoutSuggestion, many, noisySuggestions, InputMethod,
+    optionalContinuation, spacesWithoutSuggestion, many, noisySuggestions, InputMethod, Parser,
 } from "./Parser";
 import {commandDescriptions} from "./plugins/autocompletion_providers/Executable";
-import {git} from "./plugins/autocompletion_providers/Git";
 import {description, type} from "./plugins/autocompletion_providers/Suggestions";
-import {cd} from "./plugins/autocompletion_providers/Cd";
-import {alias} from "./plugins/autocompletion_providers/Alias";
+import {makeAlias} from "./plugins/autocompletion_providers/Alias";
 import {file} from "./plugins/autocompletion_providers/File";
-import {npm} from "./plugins/autocompletion_providers/NPM";
-import {rails} from "./plugins/autocompletion_providers/Rails";
 import {compose} from "./utils/Common";
+import {command} from "./plugins/autocompletion_providers/Command";
+import Aliases from "./Aliases";
 
-const ls = executable("ls");
-const exec = sequence(
-    choice(_.map(commandDescriptions, (value, key) => decorate(string(key), compose(description(value), type("executable"))))),
-    optionalContinuation(many1(file))
-);
+export const makeGrammar = (aliases: Dictionary<string>) => {
+    const exec = sequence(
+        choice(_.map(commandDescriptions, (value, key) => decorate(string(key), compose(description(value), type("executable"))))),
+        optionalContinuation(many1(file))
+    );
 
-export const command = choice([
-    ls,
-    git,
-    cd,
-    npm,
-    rails,
-]);
+    const sudo = sequence(executable("sudo"), command);
+    const anyCommand = choice([
+        sudo,
+        command,
+        exec,
+        makeAlias(aliases),
+    ]);
+    const separator = choice([
+        noisySuggestions(sequence(many(spacesWithoutSuggestion), token(string("&&")))),
+        noisySuggestions(sequence(many(spacesWithoutSuggestion), token(string(";")))),
+    ]);
 
-const sudo = sequence(executable("sudo"), command);
-const anyCommand = choice([
-    sudo,
-    command,
-    exec,
-    alias,
-]);
-const separator = choice([
-    noisySuggestions(sequence(many(spacesWithoutSuggestion), token(string("&&")))),
-    noisySuggestions(sequence(many(spacesWithoutSuggestion), token(string(";")))),
-]);
+    return sequence(sequence(anyCommand, separator), anyCommand);
+};
 
-const grammar = sequence(sequence(anyCommand, separator), anyCommand);
+export const {getSuggestions} = new class {
+    private grammar: Parser;
+    private limit = 9;
 
-const limit = 9;
+    getSuggestions = async (job: Job, inputMethod: InputMethod) => {
+        if (!this.grammar) {
+            const aliases = await Aliases.all();
+            this.grammar = makeGrammar(aliases);
+        }
 
-export async function getSuggestions(job: Job, inputMethod: InputMethod) {
-    if (window.DEBUG) {
-        /* tslint:disable:no-console */
-        console.time(`suggestion for '${job.prompt.value}'`);
-    }
+        if (window.DEBUG) {
+            /* tslint:disable:no-console */
+            console.time(`suggestion for '${job.prompt.value}'`);
+        }
 
-    const results = await grammar({
-        input: job.prompt.value,
-        directory: job.session.directory,
-        historicalCurrentDirectoriesStack: job.session.historicalCurrentDirectoriesStack,
-        cdpath: job.environment.cdpath(job.session.directory),
-        inputMethod: inputMethod,
-    });
-    const suggestions = results.map(result => result.suggestions.map(suggestion => suggestion.withPrefix(result.parse)));
-    const unique = _.uniqBy(_.flatten(suggestions), suggestion => suggestion.value).slice(0, limit);
+        const results = await this.grammar({
+            input: job.prompt.value,
+            directory: job.session.directory,
+            historicalCurrentDirectoriesStack: job.session.historicalCurrentDirectoriesStack,
+            cdpath: job.environment.cdpath(job.session.directory),
+            inputMethod: inputMethod,
+        });
 
-    if (window.DEBUG) {
-        /* tslint:disable:no-console */
-        console.timeEnd(`suggestion for '${job.prompt.value}'`);
-    }
+        const suggestions = results.map(result => result.suggestions.map(suggestion => suggestion.withPrefix(result.parse)));
+        const unique = _.uniqBy(_.flatten(suggestions), suggestion => suggestion.value).slice(0, this.limit);
 
-    return unique;
-}
+        if (window.DEBUG) {
+            /* tslint:disable:no-console */
+            console.timeEnd(`suggestion for '${job.prompt.value}'`);
+        }
+
+        return unique;
+    };
+};
--- src/plugins/autocompletion_providers/Alias.ts
+++ src/plugins/autocompletion_providers/Alias.ts
@@ -1,22 +1,17 @@
 import * as _ from "lodash";
-import Aliases from "../../Aliases";
 import {description, type} from "./Suggestions";
-import {runtime, choice, decorate, Context, string, token} from "../../Parser";
+import {choice, decorate, Context, string, token} from "../../Parser";
 import {compose} from "../../utils/Common";
-import {command} from "../../Autocompletion";
+import {command} from "./Command";
 
-export const alias = runtime(async () => {
-    const aliases = await Aliases.all();
-
-    return choice(_.map(aliases, (expanded, key) => choice([
-        decorate(token(string(key)), compose(type("alias"), description(expanded))),
-        async (context: Context) => {
-            if (context.input.startsWith(`${key} `)) {
-                const results = await command(Object.assign({}, context, {input: expanded + context.input.slice(key.length)}));
-                return results.map(result => Object.assign({}, result, {parse: key + result.parse.slice(expanded.length)}));
-            } else {
-                return [];
-            }
-        },
-    ])));
-});
+export const makeAlias = (aliases: Dictionary<string>) => choice(_.map(aliases, (expanded, key) => choice([
+    decorate(token(string(key)), compose(type("alias"), description(expanded))),
+    async (context: Context) => {
+        if (context.input.startsWith(`${key} `)) {
+            const results = await command(Object.assign({}, context, {input: expanded + context.input.slice(key.length)}));
+            return results.map(result => Object.assign({}, result, {parse: key + result.parse.slice(expanded.length)}));
+        } else {
+            return [];
+        }
+    },
+])));
