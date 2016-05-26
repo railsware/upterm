4399812df9e14ebb049f09fd566d1d69ab46abe1
Eugeny Karagodin <ekaragodin@gmail.com>
2016-05-26 09:44:16 +0600

    #361 Fix autocomplete position

--- src/views/css/main.ts
+++ src/views/css/main.ts
@@ -144,7 +144,8 @@ export namespace css {
         box: (caretOffset: Offset) => {
             return {
                 position: "absolute",
-                top: promptHeight,
+                top: caretOffset.bottom ? 'auto' : promptHeight,
+                bottom: caretOffset.bottom || 'auto',
                 left: caretOffset.left,
                 minWidth: 300,
                 boxShadow: defaultShadow,
