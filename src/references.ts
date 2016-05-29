00fd174f4cf7a298a3fd9e936c85531aaede669a
Volodymyr Shatsky <shockone89@gmail.com>
2016-05-29 10:39:14 +0300

    Set up electron-builder.

--- src/references.ts
+++ src/references.ts
@@ -3,4 +3,4 @@
 /// <reference path="../typings/Overrides.d.ts" />
 /// <reference path="../typings/Interfaces.d.ts" />
 
-/// <reference path="../node_modules/immutable/dist/immutable.d.ts" />
+/// <reference path="node_modules/immutable/dist/immutable.d.ts" />
