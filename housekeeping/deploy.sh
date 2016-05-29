40cb09c1be1a1d5516462dd2e92c2f46f1e5dcb7
Volodymyr Shatsky <shockone89@gmail.com>
2016-05-29 23:41:42 +0300

    Hide output of git push.

--- housekeeping/deploy.sh
+++ housekeeping/deploy.sh
@@ -4,5 +4,5 @@ git config user.name "Travis CI"
 git config user.email "travis@travis-ci.com"
 npm version patch -m "Bump version to %s. [ci skip]"
 npm run release
-git push "https://$GH_TOKEN:x-oauth-basic@github.com/shockone/black-screen.git" master:master
-git push "https://$GH_TOKEN:x-oauth-basic@github.com/shockone/black-screen.git" --tags
+git push --quiet "https://$GH_TOKEN:x-oauth-basic@github.com/shockone/black-screen.git" master:master > /dev/null 2>&1
+git push --quiet "https://$GH_TOKEN:x-oauth-basic@github.com/shockone/black-screen.git" --tags > /dev/null 2>&1
