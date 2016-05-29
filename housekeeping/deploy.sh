00483034578e178a8bcbe429f463bb339990c116
Volodymyr Shatsky <shockone89@gmail.com>
2016-05-30 00:09:31 +0300

    Push both head and tags.

--- housekeeping/deploy.sh
+++ housekeeping/deploy.sh
@@ -4,5 +4,4 @@ git config user.name "Travis CI"
 git config user.email "travis@travis-ci.com"
 npm version patch -m "Bump version to %s. [ci skip]"
 npm run release
-git push --quiet "https://$GH_TOKEN:x-oauth-basic@github.com/shockone/black-screen.git" HEAD:master > /dev/null 2>&1
-git push --quiet "https://$GH_TOKEN:x-oauth-basic@github.com/shockone/black-screen.git" --tags > /dev/null 2>&1
+git push --quiet "https://$GH_TOKEN:x-oauth-basic@github.com/shockone/black-screen.git" HEAD:master --tags > /dev/null 2>&1
