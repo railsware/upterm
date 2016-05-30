d9c5cca0250aa27111e7ee4564cc6c3810b6368a
Volodymyr Shatsky <shockone89@gmail.com>
2016-05-30 23:47:34 +0300

    Add debug info to deploy script. [ci skip]

--- housekeeping/deploy.sh
+++ housekeeping/deploy.sh
@@ -7,10 +7,21 @@ npm run release
 git push --quiet "https://$GH_TOKEN:x-oauth-basic@github.com/shockone/black-screen.git" HEAD:master --tags > /dev/null 2>&1
 
 TAG_NAME=$(git describe --abbrev=0)
+echo "($?) Current tag: $TAG_NAME"
+
 PREVIOUS_TAG_NAME=$(git describe --abbrev=0 --tags "$TAG_NAME^")
+echo "($?) Previous tag: $PREVIOUS_TAG_NAME"
+
 LAST_DRAFT_ID=$(curl "https://$GH_TOKEN:x-oauth-basic@api.github.com/repos/shockone/black-screen/releases" | python -c "import json,sys; array=json.load(sys.stdin); print array[0]['id'];")
+echo "($?) Last draft ID: $LAST_DRAFT_ID"
+
 BODY=$(git log --oneline --no-merges $TAG_NAME...$PREVIOUS_TAG_NAME)
+echo "($?) Body:"
+echo $BODY
+
 ESCAPED_BODY=$(echo $BODY | grep -v '\[ci skip\]' | tr '\n' '|' | sed 's/|/\\n/g' | sed 's/"/\\"/g')
+echo "($?) Escaped body:"
+echo $ESCAPED_BODY
 
 curl --request PATCH "https://$GH_TOKEN:x-oauth-basic@api.github.com/repos/shockone/black-screen/releases/$LAST_DRAFT_ID" \
     -H "Content-Type: application/json" \
