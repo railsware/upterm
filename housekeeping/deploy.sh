0f7b5fee16f0feb8e790fe7a745a1101087810bd
Volodymyr Shatsky <shockone89@gmail.com>
2016-05-31 10:47:36 +0300

    Don't strip [ci skip] lines.

--- housekeeping/deploy.sh
+++ housekeeping/deploy.sh
@@ -19,7 +19,7 @@ BODY=$(git log --oneline --no-merges $TAG_NAME...$PREVIOUS_TAG_NAME)
 echo "($?) Body:"
 echo $BODY
 
-ESCAPED_BODY=$(echo $BODY | grep -v '\[ci skip\]' | python -c "import json,sys; print json.dumps(sys.stdin.read());")
+ESCAPED_BODY=$(echo $BODY | python -c "import json,sys; print json.dumps(sys.stdin.read())[1:-1];")
 echo "($?) Escaped body:"
 echo $ESCAPED_BODY
 
