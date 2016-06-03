f8901c4846e7f8e592a1ae69e841b2ed40f56d87
Volodymyr Shatsky <shockone89@gmail.com>
2016-06-03 11:43:00 +0300

    Render on DOMContentLoaded.

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -8,8 +8,14 @@ import {ApplicationComponent} from "./1_ApplicationComponent";
 import {loadAllPlugins} from "../PluginManager";
 import {loadEnvironment} from "../Environment";
 
-// FIXME: Remove loadAllPlugins after switching to Webpack (because all the files will be loaded at start anyway).
-Promise.all([loadAllPlugins(), loadEnvironment()])
-    .then(() => reactDOM.render(<ApplicationComponent/>, document.getElementById("black-screen")));
+document.addEventListener(
+    "DOMContentLoaded",
+    () => {
+        // FIXME: Remove loadAllPlugins after switching to Webpack (because all the files will be loaded at start anyway).
+        Promise.all([loadAllPlugins(), loadEnvironment()])
+            .then(() => reactDOM.render(<ApplicationComponent/>, document.getElementById("black-screen")));
 
-Aliases.all();
+        Aliases.all();
+    },
+    false
+);
