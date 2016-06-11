3fdc495ce259f71a2186ffe95215c27043374157
Volodymyr Shatsky <shockone89@gmail.com>
2016-06-11 10:33:45 +0300

    Write a scanner prototype.

---
+++ src/shell/Scanner.ts
@@ -0,0 +1,122 @@
+abstract class Token {
+    constructor(protected raw: string) {
+    }
+
+    abstract get value(): string;
+}
+
+export class Word extends Token {
+    get value() {
+        return this.raw.trim().replace(/\\\s/, " ");
+    }
+}
+
+export class Pipe extends Token {
+    get value() {
+        return this.raw.trim();
+    }
+}
+
+export class InputRedirectionSymbol extends Token {
+    get value() {
+        return this.raw.trim();
+    }
+}
+
+export class OutputRedirectionSymbol extends Token {
+    get value() {
+        return this.raw.trim();
+    }
+}
+
+export class AppendingOutputRedirectionSymbol extends Token {
+    get value() {
+        return this.raw.trim();
+    }
+}
+
+export class StringLiteral extends Token {
+    get value() {
+        return this.raw.trim().slice(1, -1);
+    }
+}
+
+export class SingleQuotedStringLiteral extends StringLiteral {
+}
+
+export class DoubleQuotedStringLiteral extends StringLiteral {
+}
+
+export class EndOfInput extends Token {
+    get value() {
+        return this.raw.trim();
+    }
+}
+
+export function scan(input: string): Token[] {
+    const tokens: Token[] = [];
+
+    while (true) {
+        let match = input.match(/^\s*$/);
+        if (match) {
+            tokens.push(new EndOfInput(input));
+            return tokens;
+        }
+
+        match = input.match(/^(\s*\|)/);
+        if (match) {
+            const token = match[1];
+            tokens.push(new Pipe(token));
+            input = input.slice(token.length);
+            continue;
+        }
+
+        match = input.match(/^(\s*>>)/);
+        if (match) {
+            const token = match[1];
+            tokens.push(new AppendingOutputRedirectionSymbol(token));
+            input = input.slice(token.length);
+            continue;
+        }
+
+        match = input.match(/^(\s*<)/);
+        if (match) {
+            const token = match[1];
+            tokens.push(new InputRedirectionSymbol(token));
+            input = input.slice(token.length);
+            continue;
+        }
+
+        match = input.match(/^(\s*>)/);
+        if (match) {
+            const token = match[1];
+            tokens.push(new OutputRedirectionSymbol(token));
+            input = input.slice(token.length);
+            continue;
+        }
+
+        match = input.match(/^(\s*"(?:\\"|[^"])*")/);
+        if (match) {
+            const token = match[1];
+            tokens.push(new DoubleQuotedStringLiteral(token));
+            input = input.slice(token.length);
+            continue;
+        }
+
+        match = input.match(/^(\s*'(?:\\'|[^'])*')/);
+        if (match) {
+            const token = match[1];
+            tokens.push(new SingleQuotedStringLiteral(token));
+            input = input.slice(token.length);
+            continue;
+        }
+
+        match = input.match(/^(\s*(?:\\\s|[a-zA-Z0-9-=_/~.])+)/);
+        if (match) {
+            const token = match[1];
+            tokens.push(new Word(token));
+            input = input.slice(token.length);
+            continue;
+        }
+    }
+}
---
+++ test/shell/scanner_spec.ts
@@ -0,0 +1,129 @@
+import {expect} from "chai";
+import {
+    scan, EndOfInput, Word, DoubleQuotedStringLiteral, SingleQuotedStringLiteral,
+    Pipe, OutputRedirectionSymbol, AppendingOutputRedirectionSymbol, InputRedirectionSymbol,
+} from "../../src/shell/Scanner";
+
+describe.only("scan", () => {
+    it("returns end of input on empty input", () => {
+        const tokens = scan("");
+
+        expect(tokens.length).to.eq(1);
+        expect(tokens[0]).to.be.an.instanceof(EndOfInput);
+    });
+
+    it("returns end of input on input that consists only of spaces", () => {
+        const tokens = scan("  ");
+
+        expect(tokens.length).to.eq(1);
+        expect(tokens[0]).to.be.an.instanceof(EndOfInput);
+    });
+
+    it("splits on a space", () => {
+        const tokens = scan("some words");
+
+        expect(tokens.length).to.eq(3);
+        expect(tokens[0]).to.be.an.instanceof(Word);
+        expect(tokens[1]).to.be.an.instanceof(Word);
+        expect(tokens[2]).to.be.an.instanceof(EndOfInput);
+
+        expect(tokens.map(token => token.value)).to.eql(["some", "words", ""]);
+    });
+
+    it("doesn't split inside double quotes", () => {
+        const tokens = scan('prefix "inside quotes"');
+
+        expect(tokens.length).to.eq(3);
+        expect(tokens[0]).to.be.an.instanceof(Word);
+        expect(tokens[1]).to.be.an.instanceof(DoubleQuotedStringLiteral);
+        expect(tokens[2]).to.be.an.instanceof(EndOfInput);
+
+        expect(tokens.map(token => token.value)).to.eql(["prefix", "inside quotes", ""]);
+    });
+
+    it("doesn't split inside single quotes", () => {
+        const tokens = scan("prefix 'inside quotes'");
+
+        expect(tokens.length).to.eq(3);
+        expect(tokens[0]).to.be.an.instanceof(Word);
+        expect(tokens[1]).to.be.an.instanceof(SingleQuotedStringLiteral);
+        expect(tokens[2]).to.be.an.instanceof(EndOfInput);
+
+        expect(tokens.map(token => token.value)).to.eql(["prefix", "inside quotes", ""]);
+    });
+
+    it("doesn't split on an escaped space", () => {
+        const tokens = scan("prefix single\\ token");
+
+        expect(tokens.length).to.eq(3);
+        expect(tokens[0]).to.be.an.instanceof(Word);
+        expect(tokens[1]).to.be.an.instanceof(Word);
+        expect(tokens[2]).to.be.an.instanceof(EndOfInput);
+
+        expect(tokens.map(token => token.value)).to.eql(["prefix", "single token", ""]);
+    });
+
+    it("can handle special characters", () => {
+        const tokens = scan("ls --color=tty -lh");
+
+        expect(tokens.length).to.eq(4);
+        expect(tokens[0]).to.be.an.instanceof(Word);
+        expect(tokens[1]).to.be.an.instanceof(Word);
+        expect(tokens[2]).to.be.an.instanceof(Word);
+        expect(tokens[3]).to.be.an.instanceof(EndOfInput);
+
+        expect(tokens.map(token => token.value)).to.eql(["ls", "--color=tty", "-lh", ""]);
+    });
+
+    it("recognizes a pipe", () => {
+        const tokens = scan("cat file | grep word");
+
+        expect(tokens.length).to.eq(6);
+        expect(tokens[0]).to.be.an.instanceof(Word);
+        expect(tokens[1]).to.be.an.instanceof(Word);
+        expect(tokens[2]).to.be.an.instanceof(Pipe);
+        expect(tokens[3]).to.be.an.instanceof(Word);
+        expect(tokens[4]).to.be.an.instanceof(Word);
+        expect(tokens[5]).to.be.an.instanceof(EndOfInput);
+
+        expect(tokens.map(token => token.value)).to.eql(["cat", "file", "|", "grep", "word", ""]);
+    });
+
+    it("recognizes input redirection", () => {
+        const tokens = scan("cat < file");
+
+        expect(tokens.length).to.eq(4);
+        expect(tokens[0]).to.be.an.instanceof(Word);
+        expect(tokens[1]).to.be.an.instanceof(InputRedirectionSymbol);
+        expect(tokens[2]).to.be.an.instanceof(Word);
+        expect(tokens[3]).to.be.an.instanceof(EndOfInput);
+
+        expect(tokens.map(token => token.value)).to.eql(["cat", "<", "file", ""]);
+    });
+
+    it("recognizes output redirection", () => {
+        const tokens = scan("cat file > another_file");
+
+        expect(tokens.length).to.eq(5);
+        expect(tokens[0]).to.be.an.instanceof(Word);
+        expect(tokens[1]).to.be.an.instanceof(Word);
+        expect(tokens[2]).to.be.an.instanceof(OutputRedirectionSymbol);
+        expect(tokens[3]).to.be.an.instanceof(Word);
+        expect(tokens[4]).to.be.an.instanceof(EndOfInput);
+
+        expect(tokens.map(token => token.value)).to.eql(["cat", "file", ">", "another_file", ""]);
+    });
+
+    it("recognizes appending output redirection", () => {
+        const tokens = scan("cat file >> another_file");
+
+        expect(tokens.length).to.eq(5);
+        expect(tokens[0]).to.be.an.instanceof(Word);
+        expect(tokens[1]).to.be.an.instanceof(Word);
+        expect(tokens[2]).to.be.an.instanceof(AppendingOutputRedirectionSymbol);
+        expect(tokens[3]).to.be.an.instanceof(Word);
+        expect(tokens[4]).to.be.an.instanceof(EndOfInput);
+
+        expect(tokens.map(token => token.value)).to.eql(["cat", "file", ">>", "another_file", ""]);
+    });
+});
