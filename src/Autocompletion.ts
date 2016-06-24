1ca8da1f31b0734149bbd7c2d9e2f56a5cddd6ad
Volodymyr Shatsky <shockone89@gmail.com>
2016-06-24 23:42:13 +0300

    Unwrap getSuggestions from an anonymous class.

--- src/Autocompletion.ts
+++ src/Autocompletion.ts
@@ -3,18 +3,15 @@ import {leafNodeAt} from "./shell/Parser";
 
 export const suggestionsLimit = 9;
 
-export const {getSuggestions} = new class {
+export const getSuggestions = async (job: Job, caretPosition: number) => {
+    const node = leafNodeAt(caretPosition, job.prompt.ast);
+    const suggestions = await node.suggestions({
+        environment: job.environment,
+        historicalCurrentDirectoriesStack: job.session.historicalCurrentDirectoriesStack,
+        aliases: job.session.aliases,
+    });
 
-    getSuggestions = async (job: Job, caretPosition: number) => {
-        const node = leafNodeAt(caretPosition, job.prompt.ast);
-        const suggestions = await node.suggestions({
-            environment: job.environment,
-            historicalCurrentDirectoriesStack: job.session.historicalCurrentDirectoriesStack,
-            aliases: job.session.aliases,
-        });
-
-        return suggestions.filter(suggestion => tokensComparator(node.value, suggestion.value)).slice(0, suggestionsLimit);
-    };
+    return suggestions.filter(suggestion => tokensComparator(node.value, suggestion.value)).slice(0, suggestionsLimit);
 };
 
 function tokensComparator(real: string, suggested: string) {
