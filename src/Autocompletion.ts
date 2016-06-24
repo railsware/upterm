85d7d45e478cde43a014628a4ad537acfed629bc
Volodymyr Shatsky <shockone89@gmail.com>
2016-06-24 23:41:28 +0300

    Perform smart case suggestions comparison.

--- src/Autocompletion.ts
+++ src/Autocompletion.ts
@@ -13,6 +13,16 @@ export const {getSuggestions} = new class {
             aliases: job.session.aliases,
         });
 
-        return suggestions.filter(suggestion => suggestion.value.startsWith(node.value)).slice(0, suggestionsLimit);
+        return suggestions.filter(suggestion => tokensComparator(node.value, suggestion.value)).slice(0, suggestionsLimit);
     };
 };
+
+function tokensComparator(real: string, suggested: string) {
+    const shouldPerformSmartCaseComparison = real === real.toLowerCase();
+
+    if (shouldPerformSmartCaseComparison) {
+        return suggested.toLowerCase().startsWith(real);
+    } else {
+        return suggested.startsWith(real);
+    }
+}
