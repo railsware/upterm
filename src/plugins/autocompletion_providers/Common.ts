17fa03d41b4ebc51e96222021b474906f9b09bb5
Volodymyr Shatsky <shockone89@gmail.com>
2016-06-24 22:11:01 +0300

    Remove unused imports.

--- src/plugins/autocompletion_providers/Common.ts
+++ src/plugins/autocompletion_providers/Common.ts
@@ -1,16 +1,13 @@
 import {statsIn, resolveDirectory, directoryName} from "../../utils/Common";
 import {styles, Suggestion} from "./Suggestions";
-import {
-    FileInfo, SuggestionContext, AutocompletionProvider, StaticAutocompletionProvider,
-    DynamicAutocompletionProvider,
-} from "../../Interfaces";
+import {FileInfo, SuggestionContext, AutocompletionProvider} from "../../Interfaces";
 import * as Path from "path";
 
 function pathSuggestion(directory: string, path: string) {
     return new Suggestion().withValue(Path.join(directory, path).replace(/\s/g, "\\ ")).withDisplayValue(path);
 }
 
-export const filesSuggestions = (filter: (info: FileInfo) => boolean) => async (context: SuggestionContext, directory = context.environment.pwd): Promise<Suggestion[]> => {
+export const filesSuggestions = (filter: (info: FileInfo) => boolean) => async(context: SuggestionContext, directory = context.environment.pwd): Promise<Suggestion[]> => {
     const tokenDirectory = directoryName(context.argument.value);
     const directoryPath = resolveDirectory(directory, tokenDirectory);
     const stats = await statsIn(directoryPath);
@@ -27,7 +24,7 @@ export const filesSuggestions = (filter: (info: FileInfo) => boolean) => async (
 export const anyFilesSuggestions = filesSuggestions(() => true);
 export const directoriesSuggestions = filesSuggestions(info => info.stat.isDirectory());
 
-export const environmentVariableSuggestions = async (context: SuggestionContext): Promise<Suggestion[]> => {
+export const environmentVariableSuggestions = async(context: SuggestionContext): Promise<Suggestion[]> => {
     if (context.argument.value.startsWith("$")) {
         return context.environment.map((key, value) =>
             new Suggestion().withValue("$" + key).withDescription(value).withStyle(styles.environmentVariable)
@@ -37,7 +34,7 @@ export const environmentVariableSuggestions = async (context: SuggestionContext)
     }
 };
 
-export const combineAutocompletionProviders = (providers: AutocompletionProvider[]): AutocompletionProvider => async (context: SuggestionContext): Promise<Suggestion[]> => {
+export const combineAutocompletionProviders = (providers: AutocompletionProvider[]): AutocompletionProvider => async(context: SuggestionContext): Promise<Suggestion[]> => {
     let suggestions: Suggestion[] = [];
 
     for (const provider of providers) {
