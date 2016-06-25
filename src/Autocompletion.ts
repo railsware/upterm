e41cdcede9f3a4f26115004a368a7c5d93a9b0af
Volodymyr Shatsky <shockone89@gmail.com>
2016-06-25 09:16:47 +0300

    Always perform case insensitive suggestions comparison.

--- src/Autocompletion.ts
+++ src/Autocompletion.ts
@@ -3,7 +3,7 @@ import {leafNodeAt} from "./shell/Parser";
 
 export const suggestionsLimit = 9;
 
-export const getSuggestions = async (job: Job, caretPosition: number) => {
+export const getSuggestions = async(job: Job, caretPosition: number) => {
     const node = leafNodeAt(caretPosition, job.prompt.ast);
     const suggestions = await node.suggestions({
         environment: job.environment,
@@ -11,15 +11,7 @@ export const getSuggestions = async (job: Job, caretPosition: number) => {
         aliases: job.session.aliases,
     });
 
-    return suggestions.filter(suggestion => tokensComparator(node.value, suggestion.value)).slice(0, suggestionsLimit);
+    const applicableSuggestions = suggestions.filter(suggestion => suggestion.value.toLowerCase().startsWith(node.value.toLowerCase()));
+
+    return _.uniqBy(applicableSuggestions, suggestion => suggestion.value).slice(0, suggestionsLimit);
 };
-
-function tokensComparator(real: string, suggested: string) {
-    const shouldPerformSmartCaseComparison = real === real.toLowerCase();
-
-    if (shouldPerformSmartCaseComparison) {
-        return suggested.toLowerCase().startsWith(real);
-    } else {
-        return suggested.startsWith(real);
-    }
-}
