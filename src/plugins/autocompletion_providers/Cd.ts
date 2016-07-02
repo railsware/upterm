2ce10895aa943e1bdb2056a31d001f0116b6e3a1
Volodymyr Shatsky <shockone89@gmail.com>
2016-07-02 12:55:55 +0300

    Add parent folders suggestions. https://dl.dropboxusercontent.com/spa/dlqheu39w0arg9q/14m2lf8k.png

--- src/plugins/autocompletion_providers/Cd.ts
+++ src/plugins/autocompletion_providers/Cd.ts
@@ -1,11 +1,29 @@
 import {expandHistoricalDirectory} from "../../shell/Command";
 import {styles, Suggestion, directoriesSuggestionsProvider} from "./Common";
 import * as _ from "lodash";
+import * as Path from "path";
 import {PluginManager} from "../../PluginManager";
 
 PluginManager.registerAutocompletionProvider("cd", async(context) => {
     let suggestions: Suggestion[] = [];
 
+    /**
+     * Parent folders.
+     */
+    if (context.argument.value.endsWith("..")) {
+        const pwdParts = context.environment.pwd.replace(/\/$/, "").split(Path.sep);
+
+        return _.range(1, pwdParts.length).map(numberOfParts => {
+            const value = "../".repeat(numberOfParts);
+            const description = pwdParts.slice(0, -numberOfParts).join(Path.sep) || Path.sep;
+
+            return new Suggestion().withValue(value).withDescription(description).withStyle(styles.directory);
+        });
+    }
+
+    /**
+     * Historical directories.
+     */
     if (context.argument.value.startsWith("-")) {
         const historicalDirectoryAliases = _.take(["-", "-2", "-3", "-4", "-5", "-6", "-7", "-8", "-9"], context.historicalPresentDirectoriesStack.size)
             .map(alias => new Suggestion().withValue(alias).withDescription(expandHistoricalDirectory(alias, context.historicalPresentDirectoriesStack)).withStyle(styles.directory));
