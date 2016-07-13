6018d82bc40c218c1b63cac073ca96300827eb6c
Eugeny Karagodin <ekaragodin@gmail.com>
2016-07-13 10:55:08 +0600

    Autocomplete for git aliases

--- src/plugins/autocompletion_providers/Git.ts
+++ src/plugins/autocompletion_providers/Git.ts
@@ -3,9 +3,7 @@ import {styles, Suggestion, longAndShortFlag, longFlag, mapSuggestions, combine,
 import {PluginManager} from "../../PluginManager";
 import {AutocompletionProvider, AutocompletionContext} from "../../Interfaces";
 import {linedOutputOf, executeCommand} from "../../PTY";
-import {find} from 'lodash';
-
-const subCommands: Suggestion[] = [];
+import {find, flatMap} from 'lodash';
 
 async function loadAllSubcommands() {
     const text = await executeCommand("git", ["help", "-a"], process.env.HOME);
@@ -16,34 +14,37 @@ async function loadAllSubcommands() {
             .filter((match) => match.indexOf("--") === -1)
             .forEach(async(match) => {
                 const name = match.trim();
-                const suggestion = new Suggestion()
-                    .withValue(name)
-                    .withStyle(styles.command)
-                    .withSpace();
-
                 const data = find(subCommandsData, {name});
 
-                if (data) {
-                    suggestion.withDescription(data.description);
+                if (!data) {
+                    subCommandsData.push({
+                        name,
+                        description: '',
+                        aliases: [],
+                    });
                 }
-
-                subCommands.push(suggestion);
             });
     }
 }
 
 async function loadAliases() {
     const variables = await Git.configVariables(process.env.HOME);
+
     variables
         .filter(variable => variable.name.indexOf("alias.") === 0)
         .forEach(variable => {
-            const suggestion = new Suggestion()
-                .withValue(variable.name.replace("alias.", ""))
-                .withSynopsis(variable.value)
-                .withStyle(styles.command)
-                .withSpace();
+            const name = variable.name.replace("alias.", "");
+            const data = find(subCommandsData, {name: variable.value});
 
-            subCommands.push(suggestion);
+            if (data) {
+                data.aliases.push(name);
+            } else {
+                subCommandsData.push({
+                    name: name,
+                    description: variable.value,
+                    aliases: [],
+                });
+            }
         });
 }
 
@@ -62,12 +63,12 @@ const addOptions = combine([
         of the working tree. See "Interactive mode" for details.`)),
 ]);
 
-function doesLookLikeBranchAlias(word: string) {
+function doesLookLikeBranchAlias(word:string) {
     if (!word) return false;
     return word.startsWith("-") || word.includes("@") || word.includes("HEAD") || /\d/.test(word);
 }
 
-function canonizeBranchAlias(alias: string) {
+function canonizeBranchAlias(alias:string) {
     if (alias[0] === "-") {
         const steps = parseInt(alias.slice(1), 10) || 1;
         alias = `@{-${steps}}`;
@@ -137,7 +138,7 @@ const commonMergeOptions = combine([
     "no-progress",
 ].map(option => longFlag(option)));
 
-const remotes = async(context: AutocompletionContext): Promise<Suggestion[]> => {
+const remotes = async(context:AutocompletionContext):Promise<Suggestion[]> => {
     if (Git.isGitDirectory(context.environment.pwd)) {
         const names = await Git.remotes(context.environment.pwd);
         return names.map(name => new Suggestion().withValue(name).withStyle(styles.branch));
@@ -146,19 +147,19 @@ const remotes = async(context: AutocompletionContext): Promise<Suggestion[]> => 
     return [];
 };
 
-const configVariables = unique(async(context: AutocompletionContext): Promise<Suggestion[]> => {
+const configVariables = unique(async(context:AutocompletionContext):Promise<Suggestion[]> => {
     const variables = await Git.configVariables(context.environment.pwd);
 
     return variables.map((variable) => {
-        return new Suggestion()
-            .withValue(variable.name)
-            .withSynopsis(variable.value)
-            .withStyle(styles.option);
+            return new Suggestion()
+                .withValue(variable.name)
+                .withDescription(variable.value)
+                .withStyle(styles.option);
         }
     );
 });
 
-const branchesExceptCurrent = async(context: AutocompletionContext): Promise<Suggestion[]> => {
+const branchesExceptCurrent = async(context:AutocompletionContext):Promise<Suggestion[]> => {
     if (Git.isGitDirectory(context.environment.pwd)) {
         const branches = (await Git.branches(context.environment.pwd)).filter(branch => !branch.isCurrent());
         return branches.map(branch => new Suggestion().withValue(branch.toString()).withStyle(styles.branch));
@@ -167,7 +168,7 @@ const branchesExceptCurrent = async(context: AutocompletionContext): Promise<Sug
     }
 };
 
-const branchAlias = async(context: AutocompletionContext): Promise<Suggestion[]> => {
+const branchAlias = async(context:AutocompletionContext):Promise<Suggestion[]> => {
     if (doesLookLikeBranchAlias(context.argument.value)) {
         let nameOfAlias = (await linedOutputOf("git", ["name-rev", "--name-only", canonizeBranchAlias(context.argument.value)], context.environment.pwd))[0];
         if (nameOfAlias && !nameOfAlias.startsWith("Could not get")) {
@@ -178,7 +179,7 @@ const branchAlias = async(context: AutocompletionContext): Promise<Suggestion[]>
     return [];
 };
 
-const notStagedFiles = unique(async(context: AutocompletionContext): Promise<Suggestion[]> => {
+const notStagedFiles = unique(async(context:AutocompletionContext):Promise<Suggestion[]> => {
     if (Git.isGitDirectory(context.environment.pwd)) {
         const fileStatuses = await Git.status(context.environment.pwd);
         return fileStatuses.map(fileStatus => new Suggestion().withValue(fileStatus.value).withStyle(styles.gitFileStatus(fileStatus.code)));
@@ -188,104 +189,127 @@ const notStagedFiles = unique(async(context: AutocompletionContext): Promise<Sug
 });
 
 interface GitCommandData {
-    name: string;
-    description: string;
-    provider?: AutocompletionProvider
+    name:string;
+    description:string;
+    provider?:AutocompletionProvider;
+    aliases:string[];
 }
 
-const subCommandsData: GitCommandData[] = [
+const subCommandsData:GitCommandData[] = [
     {
         name: "add",
         description: "Add file contents to the index.",
         provider: combine([notStagedFiles, addOptions]),
+        aliases: [],
     },
     {
         name: "am",
         description: "Apply a series of patches from a mailbox.",
+        aliases: [],
     },
     {
         name: "archive",
         description: "Create an archive of files from a named tree.",
+        aliases: [],
     },
     {
         name: "bisect",
         description: "Find by binary search the change that introduced a bug.",
+        aliases: [],
     },
     {
         name: "branch",
         description: "List, create, or delete branches.",
+        aliases: [],
     },
     {
         name: "bundle",
         description: "Move objects and refs by archive.",
+        aliases: [],
     },
     {
         name: "checkout",
         description: "Switch branches or restore working tree files.",
         provider: combine([branchesExceptCurrent, branchAlias, notStagedFiles]),
+        aliases: [],
     },
     {
         name: "cherry-pick",
         description: "Apply the changes introduced by some existing commits.",
+        aliases: [],
     },
     {
         name: "citool",
         description: "Graphical alternative to git-commit.",
+        aliases: [],
     },
     {
         name: "clean",
         description: "Remove untracked files from the working tree.",
+        aliases: [],
     },
     {
         name: "clone",
         description: "Clone a repository into a new directory.",
+        aliases: [],
     },
     {
         name: "commit",
         description: "Record changes to the repository.",
         provider: commitOptions,
+        aliases: [],
     },
     {
         name: "config",
         description: "Get and set repository or global options",
         provider: combine([configVariables, configOptions]),
+        aliases: [],
     },
     {
         name: "describe",
         description: "Describe a commit using the most recent tag reachable from it.",
+        aliases: [],
     },
     {
         name: "diff",
         description: "Show changes between commits, commit and working tree, etc.",
+        aliases: [],
     },
     {
         name: "fetch",
         description: "Download objects and refs from another repository.",
         provider: combine([remotes, fetchOptions]),
+        aliases: [],
     },
     {
         name: "format-patch",
         description: "Prepare patches for e-mail submission.",
+        aliases: [],
     },
     {
         name: "gc",
         description: "Cleanup unnecessary files and optimize the local repository.",
+        aliases: [],
     },
     {
         name: "grep",
         description: "Print lines matching a pattern.",
+        aliases: [],
     },
     {
         name: "gui",
         description: "A portable graphical interface to Git.",
+        aliases: [],
     },
     {
         name: "init",
         description: "Create an empty Git repository or reinitialize an existing one.",
+        aliases: [],
     },
     {
         name: "log",
         description: "Show commit logs.",
+        aliases: [],
     },
     {
         name: "merge",
@@ -298,14 +322,17 @@ const subCommandsData: GitCommandData[] = [
             longFlag("no-rerere-autoupdate"),
             longFlag("abort"),
         ]),
+        aliases: [],
     },
     {
         name: "mv",
         description: "Move or rename a file, a directory, or a symlink.",
+        aliases: [],
     },
     {
         name: "notes",
         description: "Add or inspect object notes.",
+        aliases: [],
     },
     {
         name: "pull",
@@ -316,67 +343,106 @@ const subCommandsData: GitCommandData[] = [
             commonMergeOptions,
             fetchOptions,
         ]),
+        aliases: [],
     },
     {
         name: "push",
         description: "Update remote refs along with associated objects.",
         provider: pushOptions,
+        aliases: [],
     },
     {
         name: "rebase",
         description: "Forward-port local commits to the updated upstream head.",
+        aliases: [],
     },
     {
         name: "reset",
         description: "Reset current HEAD to the specified state.",
+        aliases: [],
     },
     {
         name: "revert",
         description: "Revert some existing commits.",
+        aliases: [],
     },
     {
         name: "rm",
         description: "Remove files from the working tree and from the index.",
+        aliases: [],
     },
     {
         name: "shortlog",
         description: "Summarize git log output.",
+        aliases: [],
     },
     {
         name: "show",
         description: "Show various types of objects.",
+        aliases: [],
     },
     {
         name: "stash",
         description: "Stash the changes in a dirty working directory away.",
+        aliases: [],
     },
     {
         name: "status",
         description: "Show the working tree status.",
         provider: statusOptions,
+        aliases: [],
     },
     {
         name: "submodule",
         description: "Initialize, update or inspect submodules.",
+        aliases: [],
     },
     {
         name: "tag",
         description: "Create, list, delete or verify a tag object signed with GPG.",
+        aliases: [],
     },
     {
         name: "worktree",
         description: "Manage multiple worktrees.",
+        aliases: [],
     },
 ];
 
 PluginManager.registerAutocompletionProvider("git", context => {
     if (context.argument.position === 1) {
-        return subCommands;
+        return flatMap(subCommandsData, data => {
+            const result = [
+                new Suggestion()
+                    .withValue(data.name)
+                    .withDescription(data.description)
+                    .withStyle(styles.command)
+                    .withSpace()
+            ];
+
+            data.aliases.forEach(name => {
+                const aliasSuggestion = new Suggestion()
+                    .withValue(name)
+                    .withDescription(data.description)
+                    .withStyle(styles.command)
+                    .withSpace();
+
+                result.push(aliasSuggestion);
+            });
+
+            return result;
+        });
     } else {
         const firstArgument = context.argument.command.nthArgument(1);
 
         if (firstArgument) {
-            const data = find(subCommandsData, {name: firstArgument.value});
+            const data = find(subCommandsData, (data) => {
+                if (data.name === firstArgument.value) {
+                    return true;
+                }
+
+                return data.aliases.indexOf(firstArgument.value) !== -1;
+            });
 
             if (data && data.provider) {
                 return data.provider(context);
