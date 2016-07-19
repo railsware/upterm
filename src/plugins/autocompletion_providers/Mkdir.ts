31d39d4497c8175ccefdeb17d17fa4e0db70b7b0
Drew Gross <drew.a.gross@gmail.com>
2016-07-18 22:50:32 -0700

    DRY some code

--- src/plugins/autocompletion_providers/Mkdir.ts
+++ src/plugins/autocompletion_providers/Mkdir.ts
@@ -3,6 +3,19 @@ import {shortFlag, mapSuggestions, combine, directoriesSuggestionsProvider} from
 import {mapObject} from "../../utils/Common";
 import {exec} from "child_process";
 
+const combineManPageLines = (lines: string[]) => lines
+    .map(line => line.trim())
+    .reduce(
+        (memo, next) => {
+            if (next.endsWith("-")) {
+                return memo.concat(next.slice(0, -1));
+            } else {
+                return memo.concat(next, " ");
+            }
+        },
+        ""
+    );
+
 exec("man mkdir", {}, (error: any, stdout: string, stderr: string) => {
     // "Apply" backspace literals
     const manContents = stdout.replace(/.\x08/g, "");
@@ -42,32 +55,9 @@ exec("man mkdir", {}, (error: any, stdout: string, stderr: string) => {
         let shortFlagWithArgument = description[0].match(/^ *-(\w \w*)$/);
         let shortFlagWithoutArgument = description[0].match(/^ *-(\w) *(.*)$/);
         if (shortFlagWithArgument) {
-            flagMap[shortFlagWithArgument[1]] = description
-                .slice(1)
-                .map(line => line.trim())
-                .reduce(
-                    (memo, next) => {
-                        if (next.endsWith("-")) {
-                            return memo.concat(next.slice(0, -1));
-                        } else {
-                            return memo.concat(next, " ");
-                        }
-                    },
-                    ""
-                );
+            flagMap[shortFlagWithArgument[1]] = combineManPageLines(description.slice(1));
         } else if (shortFlagWithoutArgument) {
-            flagMap[shortFlagWithoutArgument[1]] = [shortFlagWithoutArgument[2], ...description.slice(1)]
-                .map(line => line.trim())
-                .reduce(
-                    (memo, next) => {
-                        if (next.endsWith("-")) {
-                            return memo.concat(next.slice(0, -1));
-                        } else {
-                            return memo.concat(next, " ");
-                        }
-                    },
-                    ""
-                );
+            flagMap[shortFlagWithoutArgument[1]] = combineManPageLines([shortFlagWithoutArgument[2], ...description.slice(1)]);
         }
     });
     const optionsProvider = mapObject(flagMap, (option, descriptions) => mapSuggestions(shortFlag(option), suggestion => suggestion.withDescription(descriptions)));
