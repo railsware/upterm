c00a6fb2f1f00de36830f0438972a1e69afe58a4
Drew Gross <drew.a.gross@gmail.com>
2017-01-21 19:25:57 -0800

    Fetch only latest releast, for more speed

--- housekeeping/deploy.sh
+++ housekeeping/deploy.sh
@@ -12,7 +12,7 @@ echo "($?) Current tag: $TAG_NAME"
 PREVIOUS_TAG_NAME=$(git describe --abbrev=0 --tags "$TAG_NAME^")
 echo "($?) Previous tag: $PREVIOUS_TAG_NAME"
 
-LAST_DRAFT_ID=$(curl "https://$GH_TOKEN:x-oauth-basic@api.github.com/repos/vshatskyi/black-screen/releases" | python -c "import json,sys; array=json.load(sys.stdin); print array[0]['id'];")
+LAST_DRAFT_ID=$(curl "https://$GH_TOKEN:x-oauth-basic@api.github.com/repos/vshatskyi/black-screen/releases/latest" | python -c "import json,sys; obj=json.load(sys.stdin); print obj['id'];")
 echo "($?) Last draft ID: $LAST_DRAFT_ID"
 
 BODY=$(git log --oneline --no-merges $TAG_NAME...$PREVIOUS_TAG_NAME | python -c "import json,sys; print json.dumps(sys.stdin.read());")
