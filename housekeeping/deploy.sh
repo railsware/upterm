1235abea56971c47b295636c2c0c9342c8e851c4
Drew Gross <drew.a.gross@gmail.com>
2017-03-05 18:40:59 -0800

    Remove most deploy steps so I can debug them

--- housekeeping/deploy.sh
+++ housekeeping/deploy.sh
@@ -5,21 +5,21 @@ git config user.name "Travis CI"
 git config user.email "travis@travis-ci.com"
 npm version patch -m "Bump version to %s. [ci skip]"
 npm run release
-git push --quiet "https://$GH_TOKEN:x-oauth-basic@github.com/vshatskyi/black-screen.git" HEAD:master --tags > /dev/null 2>&1
+# git push --quiet "https://$GH_TOKEN:x-oauth-basic@github.com/vshatskyi/black-screen.git" HEAD:master --tags > /dev/null 2>&1
 
-NEW_RELEASE_TAG=$(git describe --abbrev=0)
-echo "($?) Current release tag: $NEW_RELEASE_TAG"
+# NEW_RELEASE_TAG=$(git describe --abbrev=0)
+# echo "($?) Current release tag: $NEW_RELEASE_TAG"
 
-PREVIOUS_RELEASE_TAG=$(git describe --abbrev=0 --tags "$NEW_RELEASE_TAG^")
-echo "($?) Previous release tag: $PREVIOUS_RELEASE_TAG"
+# PREVIOUS_RELEASE_TAG=$(git describe --abbrev=0 --tags "$NEW_RELEASE_TAG^")
+# echo "($?) Previous release tag: $PREVIOUS_RELEASE_TAG"
 
-NEW_RELEASE_LAST_DRAFT_ID=$(curl "https://$GH_TOKEN:x-oauth-basic@api.github.com/repos/vshatskyi/black-screen/releases/latest" | python -c "import json,sys; obj=json.load(sys.stdin); print obj['id'];")
-echo "($?) New release draft ID: $NEW_RELEASE_LAST_DRAFT_ID"
+# NEW_RELEASE_LAST_DRAFT_ID=$(curl "https://$GH_TOKEN:x-oauth-basic@api.github.com/repos/vshatskyi/black-screen/releases/latest" | python -c "import json,sys; obj=json.load(sys.stdin); print obj['id'];")
+# echo "($?) New release draft ID: $NEW_RELEASE_LAST_DRAFT_ID"
 
-NEW_RELEASE_BODY=$(git log --oneline --no-merges $NEW_RELEASE_TAG...$PREVIOUS_RELEASE_TAG | python -c "import json,sys; print json.dumps(sys.stdin.read());")
-echo "($?) Body:"
-echo $NEW_RELEASE_BODY
+# NEW_RELEASE_BODY=$(git log --oneline --no-merges $NEW_RELEASE_TAG...$PREVIOUS_RELEASE_TAG | python -c "import json,sys; print json.dumps(sys.stdin.read());")
+# echo "($?) Body:"
+# echo $NEW_RELEASE_BODY
 
-curl --request PATCH "https://$GH_TOKEN:x-oauth-basic@api.github.com/repos/vshatskyi/black-screen/releases/$NEW_RELEASE_LAST_DRAFT_ID" \
-    -H "Content-Type: application/json" \
-    -d "{\"body\": $NEW_RELEASE_BODY, \"draft\": false, \"prerelease\": false, \"tag_name\": \"$NEW_RELEASE_TAG\"}"
+# curl --request PATCH "https://$GH_TOKEN:x-oauth-basic@api.github.com/repos/vshatskyi/black-screen/releases/$NEW_RELEASE_LAST_DRAFT_ID" \
+#     -H "Content-Type: application/json" \
+#     -d "{\"body\": $NEW_RELEASE_BODY, \"draft\": false, \"prerelease\": false, \"tag_name\": \"$NEW_RELEASE_TAG\"}"
