56057d9a973d57f63a6a8367a119f309f861d349
Drew Gross <drew.a.gross@gmail.com>
2017-03-05 19:21:48 -0800

    Push tags before running release

--- housekeeping/deploy.sh
+++ housekeeping/deploy.sh
@@ -2,8 +2,8 @@
 git config user.name "Travis CI"
 git config user.email "travis@travis-ci.com"
 npm version patch -m "Bump version to %s. [ci skip]"
+git push --quiet "https://$GH_TOKEN:x-oauth-basic@github.com/vshatskyi/black-screen.git" HEAD:master --tags > /dev/null 2>&1
 npm run release
-# git push --quiet "https://$GH_TOKEN:x-oauth-basic@github.com/vshatskyi/black-screen.git" HEAD:master --tags > /dev/null 2>&1
 
 # NEW_RELEASE_TAG=$(git describe --abbrev=0)
 # echo "($?) Current release tag: $NEW_RELEASE_TAG"
