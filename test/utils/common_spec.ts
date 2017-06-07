00830538f7266f2bb9d72a7924d23e5e2ca5c6ce
Volodymyr Shatskyi <shockone89@gmail.com>
2017-06-07 09:50:42 +0300

    Add fuzzyMatch tests.

--- test/utils/common_spec.ts
+++ test/utils/common_spec.ts
@@ -1,12 +1,22 @@
 import "mocha";
 import {expect} from "chai";
-import {commonPrefix} from "../../src/utils/Common";
+import {commonPrefix, fuzzyMatch} from "../../src/utils/Common";
 
 
 describe("common utils", () => {
     describe("commonPrefix", () => {
-        it("returns the whole string for the same strings", async() => {
+        it("returns the whole string for the same strings", () => {
             expect(commonPrefix("abc", "abc")).to.eql("abc");
+        });
+    });
+
+    describe("fuzzyMatch", () => {
+        it("matches beginning of string", () => {
+            expect(fuzzyMatch("com", "commit")).to.eql(true);
+        });
+
+        it("matches beginning of token", () => {
+            expect(fuzzyMatch("nam", "file_name")).to.eql(true);
         });
     });
 });
