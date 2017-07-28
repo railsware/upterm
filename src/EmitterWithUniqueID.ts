4c1d2776894fc9684ec1d0c6c78fad97c92ab3b6
Volodymyr Shatskyi <shockone89@gmail.com>
2017-07-28 17:36:08 +0300

    Use timestamp as Session and Job ID.

--- src/EmitterWithUniqueID.ts
+++ src/EmitterWithUniqueID.ts
@@ -1,13 +1,10 @@
 import * as events from "events";
 
-let nextId = 0;
-
 export class EmitterWithUniqueID extends events.EventEmitter {
     public id: number;
 
     constructor() {
         super();
-        this.id = nextId;
-        nextId++;
+        this.id = Date.now();
     }
 }
