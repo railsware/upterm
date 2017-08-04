4b8edab870ee4a4002bcf33c93d8892814799368
Volodymyr Shatskyi <shockone89@gmail.com>
2017-08-04 20:03:52 +0300

    Extract observable creation.

--- src/services/GitService.ts
+++ src/services/GitService.ts
@@ -24,22 +24,24 @@ async function getState(directory: string): Promise<GitState> {
     }
 }
 
+function createObservable(directory: string) {
+    return Observable
+        .timer(0, INTERVAL)
+        .concatMap(() => getState(directory))
+        // Don't emit if a value didn't change.
+        .distinct(JSON.stringify)
+        // Remember the last value to emit immediately to new subscriptions.
+        .multicast(new BehaviorSubject<GitState>({kind: "not-repository"}))
+        // Automatically stop checking git status when there are no subscriptions anymore.
+        .refCount();
+}
+
 export class GitService {
     private observables: Map<string, Observable<GitState>> = new Map();
 
     observableFor(directory: string): Observable<GitState> {
         if (!this.observables.has(directory)) {
-            const observable = Observable
-                .timer(0, INTERVAL)
-                .concatMap(() => getState(directory))
-                // Don't emit if a value didn't change.
-                .distinct(JSON.stringify)
-                // Remember the last value to emit immediately to new subscriptions.
-                .multicast(new BehaviorSubject<GitState>({kind: "not-repository"}))
-                // Automatically stop checking git status when there are no subscriptions anymore.
-                .refCount();
-
-            this.observables.set(directory, observable);
+            this.observables.set(directory, createObservable(directory));
         }
 
         return this.observables.get(directory)!;
