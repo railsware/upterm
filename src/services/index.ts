d738e2aae760d793ed3742aa1653e6f1d379f7e8
Volodymyr Shatskyi <shockone89@gmail.com>
2017-08-04 17:24:56 +0300

    .

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,16 +1,19 @@
 import {FontService} from "./FontService";
 import {HistoryService} from "./HistoryService";
 import {UpdatesService} from "./UpdatesService";
+import {GitService} from "./GitService";
 
 // To help IDE with "find usages" and "go to definition".
 interface Services {
     font: FontService;
     history: HistoryService;
     updates: UpdatesService;
+    git: GitService;
 }
 
 export const services: Services = {
     font: FontService.instance,
     history: HistoryService.instance,
     updates: UpdatesService.instance,
+    git: new GitService(),
 };
