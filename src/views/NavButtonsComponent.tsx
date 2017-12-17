4485f92fd005fb90549ca377b8f7094959bb7c08
Lucas Sindra <lucassindra01@hotmail.com>
2017-12-17 00:46:47 -0200

    Hidden window title bar and created custom min/max/close buttons

---
+++ src/views/NavButtonsComponent.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+
+export interface Props {
+    minimize: React.EventHandler<React.MouseEvent<HTMLSpanElement>>;
+    maximize: React.EventHandler<React.MouseEvent<HTMLSpanElement>>;
+    close: React.EventHandler<React.MouseEvent<HTMLSpanElement>>;
+}
+
+export class NavButtonsComponent extends React.Component<Props, {}> {
+    constructor() {
+        super();
+    }
+    render() {
+        return (
+            <div className="navButtons">
+                <button id="min-btn" onClick={this.props.minimize}>-</button>
+                <button id="max-btn" onClick={this.props.maximize}>+</button>
+                <button id="close-btn" onClick={this.props.close}>x</button>
+            </div>
+        );
+    }
+}
